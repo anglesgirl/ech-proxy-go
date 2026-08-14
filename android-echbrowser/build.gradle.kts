e59bd7e771407dfc0ad6b641bceb03638606ddc7
anglesgirl <anglesgirl@users.noreply.github.com>
2026-08-14 17:45:17 +0800

    fix: align with gecko-ech-demo - Kotlin 2.2.0 + JDK 17 (stable combo)

--- android-echbrowser/build.gradle.kts
+++ android-echbrowser/build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
 }
