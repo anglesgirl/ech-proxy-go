02039127ded1ced5630001b32cee17e089369378
anglesgirl <anglesgirl@users.noreply.github.com>
2026-08-20 23:20:23 +0800

    fix: resolve Android SDK runtime dependencies

--- mobile/ech-android/settings.gradle.kts
+++ mobile/ech-android/settings.gradle.kts
@@ -9,4 +9,12 @@ buildscript {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "ech-android"
